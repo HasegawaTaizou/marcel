15d502b3c1076e47f29e007368569b2f368f40b9
HasegawaTaizou <caiocoghi@gmail.com>
2023-02-13 02:08:50 -0200

    d

--- AULA06/exercicio2.js
+++ AULA06/exercicio2.js
@@ -8,16 +8,52 @@ var entradaDados = readline.createInterface({
 });
 
 entradaDados.question("Digite a tabuada inicial: ", function (tabuada) {
-  let tabuadaInicial = tabuada;
+  let tabuadaInicial;
+  if (
+    moduloExercicio2.verificarValorVazio(tabuada) &&
+    moduloExercicio2.verificarValorTabuadaInicial(tabuada)
+  ) {
+    tabuadaInicial = tabuada;
+  } else {
+    console.log("DIGITE A TABUADA INICIAL");
+    entradaDados.close();
+  }
 
   entradaDados.question("Digite a tabuada final: ", function (tabuada) {
-    let tabuadaFinal = tabuada;
+    let tabuadaFinal;
+    if (
+      moduloExercicio2.verificarValorVazio(tabuada) &&
+      moduloExercicio2.verificarValorTabuadaFinal(tabuada)
+    ) {
+      tabuadaFinal = tabuada;
+    } else {
+      console.log("DIGITE A TABUADA FINAL");
+      entradaDados.close();
+    }
 
     entradaDados.question("Digite o contador inicial: ", function (contador) {
-      let contadorInicial = contador;
+      let contadorInicial;
+      if (
+        moduloExercicio2.verificarValorVazio(contador) &&
+        moduloExercicio2.verificarValorContadorInicial(contador)
+      ) {
+        contadorInicial = contador;
+      } else {
+        console.log("DIGITE O CONTADOR INICIAL");
+        entradaDados.close();
+      }
 
       entradaDados.question("Digite o contador final: ", function (contador) {
-        let contadorFinal = contador;
+        let contadorFinal;
+        if (
+          moduloExercicio2.verificarValorVazio(contador) &&
+          moduloExercicio2.verificarValorContadorFinal(contador)
+        ) {
+          contadorFinal = contador;
+        } else {
+          console.log("DIGITE O CONTADOR FINAL");
+          entradaDados.close();
+        }
 
         moduloExercicio2.mostrarResultado(
           tabuadaInicial,
--- AULA06/modulo/modulo-exercicio2.js
+++ AULA06/modulo/modulo-exercicio2.js
@@ -26,6 +26,54 @@ const mostrarResultado = function (
   }
 };
 
+const verificarValorVazio = function (numero) {
+  if (numero == "") {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+// A entrada da tabuada deverá ser entre 2 e 100, não sendo permitido
+// outros valores;
+const verificarValorTabuadaInicial = function (tabuadaInicial) {
+  if (Number(tabuadaInicial) >= 2 && Number(tabuadaInicial) <= 100) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const verificarValorTabuadaFinal = function (tabuadaFinal) {
+  if (Number(tabuadaFinal) >= 2 && Number(tabuadaFinal) <= 100) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+//O valor até onde será calculada a tabuada deverá ser entre 1 e 50;
+const verificarValorContadorInicial = function (contadorInicial) {
+  if (Number(contadorInicial) >= 1 && Number(contadorInicial) <= 50) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const verificarValorContadorFinal = function (contadorFinal) {
+  if (Number(contadorFinal) >= 1 && Number(contadorFinal) <= 50) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 module.exports = {
   mostrarResultado,
+  verificarValorVazio,
+  verificarValorTabuadaInicial,
+  verificarValorTabuadaFinal,
+  verificarValorContadorInicial,
+  verificarValorContadorFinal,
 };
