61c5e3c7d0a250325da624d277453b893b875986
HasegawaTaizou <caiocoghi@gmail.com>
2023-02-14 23:14:15 -0200

    exercicio 2 refatorado

--- AULA06/exercicio2.js
+++ AULA06/exercicio2.js
@@ -9,62 +9,56 @@ var entradaDados = readline.createInterface({
 
 entradaDados.question("Digite a tabuada inicial: ", function (tabuada) {
   let tabuadaInicial;
+
   if (
-    moduloExercicio2.verificarValorVazio(tabuada)
-     &&
-    moduloExercicio2.verificarValorTabuadaInicial(tabuada)
+    moduloExercicio2.verificarValorVazio(tabuada) &&
+    moduloExercicio2.verificarValorTabuada(tabuada)
   ) {
     tabuadaInicial = tabuada;
   } else {
-    console.log("DIGITE A TABUADA INICIAL");
     entradaDados.close();
+    return false;
   }
 
   entradaDados.question("Digite a tabuada final: ", function (tabuada) {
     let tabuadaFinal;
+
     if (
       moduloExercicio2.verificarValorVazio(tabuada) &&
-      moduloExercicio2.verificarValorTabuadaFinal(tabuada)
-    ) 
-    {
+      moduloExercicio2.verificarValorTabuada(tabuada) &&
+      moduloExercicio2.verificarValorMaior(tabuadaInicial, tabuada)
+    ) {
       tabuadaFinal = tabuada;
-      if(moduloExercicio2.verificarValorMaior(tabuadaInicial, tabuadaFinal)) {
-      } else {
-        entradaDados.close()
-        console.log('DIGITE A TABUADA INICIAL COM UM VALOR MENOR QUE A TABUADA FINAL');
-      }
     } else {
       entradaDados.close();
-      console.log("DIGITE A TABUADA FINAL");
+      return false;
     }
 
     entradaDados.question("Digite o contador inicial: ", function (contador) {
       let contadorInicial;
+
       if (
         moduloExercicio2.verificarValorVazio(contador) &&
-        moduloExercicio2.verificarValorContadorInicial(contador)
+        moduloExercicio2.verificarValorContador(contador)
       ) {
         contadorInicial = contador;
       } else {
         entradaDados.close();
-        console.log("DIGITE O CONTADOR INICIAL");
+        return false;
       }
 
       entradaDados.question("Digite o contador final: ", function (contador) {
         let contadorFinal;
+
         if (
           moduloExercicio2.verificarValorVazio(contador) &&
-          moduloExercicio2.verificarValorContadorFinal(contador)
+          moduloExercicio2.verificarValorContador(contador) &&
+          moduloExercicio2.verificarValorMaior(contadorInicial, contador)
         ) {
           contadorFinal = contador;
-          if(moduloExercicio2.verificarValorMaior(contadorInicial, contadorFinal)) {
-          } else {
-            console.log('DIGITE O CONTADOR INICIAL COM UM VALOR MENOR QUE O CONTADOR FINAL');
-            entradaDados.close()
-          }
         } else {
-          console.log("DIGITE O CONTADOR FINAL");
           entradaDados.close();
+          return false;
         }
 
         moduloExercicio2.mostrarResultado(
--- AULA06/modulo/modulo-exercicio2.js
+++ AULA06/modulo/modulo-exercicio2.js
@@ -9,6 +9,7 @@ const mostrarResultado = function (
     contadorInicialResetado = contadorInicial;
     contadorFinalResetado = contadorFinal;
 
+    console.log(`Tabuada do [${tabuadaInicial}]`);
     while (contadorInicial <= contadorFinal) {
       resultado = Number(tabuadaInicial) * Number(contadorInicial);
       console.log(`${tabuadaInicial} x ${contadorInicial} = ${resultado}`);
@@ -19,67 +20,48 @@ const mostrarResultado = function (
 
     tabuadaInicial++;
   }
-  if (tabuadaInicial <= tabuadaFinal) {
-    console.log(`${tabuadaInicial} x ${contadorInicial} = ${resultado}`);
-  } else {
-    console.log("CABOU");
-  }
 };
 
 const verificarValorVazio = function (numero) {
   if (numero == "") {
+    console.log("VALOR VAZIO. PREENCHA COM UM NÚMERO VÁLIDO");
     return false;
   } else {
-    return true;
+    return numero;
   }
 };
 
-const verificarValorTabuadaInicial = function (tabuadaInicial) {
-  if (Number(tabuadaInicial) >= 2 && Number(tabuadaInicial) <= 100) {
-    return true;
+const verificarValorTabuada = function (tabuada) {
+  if (Number(tabuada) >= 2 && Number(tabuada) <= 100) {
+    return tabuada;
   } else {
-    return false;
-  }
-};
-
-const verificarValorTabuadaFinal = function (tabuadaFinal) {
-  if (Number(tabuadaFinal) >= 2 && Number(tabuadaFinal) <= 100) {
-    return true;
-  } else {
+    console.log("TABUADA FORA DO LIMITE. PREENCHA COM UM NÚMERO CORRETO");
     return false;
   }
 };
 
-const verificarValorContadorInicial = function (contadorInicial) {
-  if (Number(contadorInicial) >= 1 && Number(contadorInicial) <= 50) {
-    return true;
+const verificarValorContador = function (contador) {
+  if (Number(contador) >= 1 && Number(contador) <= 50) {
+    return contador;
   } else {
-    return false;
-  }
-};
-
-const verificarValorContadorFinal = function (contadorFinal) {
-  if (Number(contadorFinal) >= 1 && Number(contadorFinal) <= 50) {
-    return true;
-  } else {
+    console.log("CONTADOR FORA DO LIMITE. PREENCHA COM UM NÚMERO CORRETO");
     return false;
   }
 };
 
 const verificarValorMaior = function (valor1, valor2) {
-  if(valor1 > valor2) {
-    return false
+  if (valor1 > valor2) {
+    console.log("VALOR INICIAL MAIOR QUE O FINAL. PREENCHA CORRETAMENTE");
+    return false;
   } else {
-    return true
+    return true;
   }
-}
+};
 
 module.exports = {
   mostrarResultado,
   verificarValorVazio,
-  verificarValorTabuadaInicial,
-  verificarValorTabuadaFinal,
-  verificarValorContadorInicial,
-  verificarValorContadorFinal,
+  verificarValorTabuada,
+  verificarValorContador,
   verificarValorMaior,
 };
