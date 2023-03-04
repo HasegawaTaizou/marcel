16621a763a72390ef73569a8805f5d636f12ec91
HasegawaTaizou <caiocoghi@gmail.com>
2023-03-04 10:46:59 -0300

    tudo feito

--- AULA08/get-capital-pais.js
+++ AULA08/get-capital-pais.js
@@ -1,23 +1,29 @@
-const estadosCidades = require('./estados_cidades.js')
+const estadosCidades = require("./estados_cidades.js");
 
-// console.log(estadosCidades);
+const getCapitalPais = function () {
+  const listaEstados = estadosCidades.estadosCidades.estados;
 
-const listaEstados = estadosCidades.estadosCidades.estados
+  let capitaisFiltradas = listaEstados.filter((item) => {
+    return item.hasOwnProperty("capital_pais");
+  });
 
-let capitaisFiltradas = listaEstados.filter(item => {
-    return item.hasOwnProperty('capital_pais');
-})
+  let capitais = {};
+  let capitaisArray = [];
+  let capital = {};
 
-let capital = {}
+  capitaisFiltradas.forEach((item) => {
+    capital.capital_atual = item.capital_pais.capital;
+    capital.uf = item.sigla;
+    capital.descricao = item.nome;
+    capital.regiao = item.regiao;
+    capital.capital_pais_ano_inicio = item.capital_pais.ano_inicio;
+    capital.capital_pais_ano_termino = item.capital_pais.ano_fim;
 
-let capitais = capitaisFiltradas.forEach((item, index) => {
-    capital.capital_atual = item.capital_pais.capital
-    capital.uf = item.sigla
-    capital.descricao = item.nome
-    capital.regiao = item.regiao
-    capital.capital_pais_ano_inicio = item.capital_pais.ano_inicio
-    capital.capital_pais_ano_termino = item.capital_pais.ano_fim
+    capitaisArray.push(capital);
+  });
 
-    console.log(capital);
-    
-})
+  capitais.capitais = capitaisArray;
+  console.log(capitais);
+};
+
+getCapitalPais();
